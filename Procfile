f2385330f08ef884b3d32598a8ea517ec6233832
Eduard Ragea <eduard.ragea@kcl.ac.uk>
2023-01-31 21:43:07 +0200

    Prepare application for heroku deployment.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bankinggame.wsgi
--- bank/settings.py
+++ bank/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from django.contrib.messages import constants as message_constants
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -143,3 +145,5 @@ MESSAGE_TAGS = {
     message_constants.DEBUG: 'dark',
     message_constants.ERROR: 'danger',
 }
+
+django_heroku.settings(locals())
